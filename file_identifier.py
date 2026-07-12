ecb2677d5f2ca64158d02ac567e5f6ee33fb7c6d
Clem <104423339+ClementCoiffier@users.noreply.github.com>
2026-07-12 18:55:44 -0400

    fonctionnalité: ajout d'une base de données de signatures de nombres magiques

--- file_identifier.py
+++ file_identifier.py
@@ -1,4 +1,13 @@
 import sys
+import json
+
+def load_signatures(db_path="signatures.json"):
+    """Charge la base de signatures et convertit les magic en bytes."""
+    with open(db_path, "r") as f:
+        data= json.load(f)  #dictionary of one key: "signatures" and a list of values
+    for sig in data["signatures"]: #item from the collection "sigs"
+        sig["magic_bytes"] = bytes.fromhex(sig["magic"]) #conversion of bytes from hex
+    return data["signatures"]
 
 def read_header(filepath, num_bytes=16):
     """Lit les premiers octets d'un fichier en mode binaire."""
@@ -6,8 +15,12 @@ def read_header(filepath, num_bytes=16):
         return f.read(num_bytes) #f.read(16)=retourne un objet bytes pas string
     
 if __name__ == "__main__":
+    sigs= load_signatures() #complete list of dictionaries
+    print(f"{len(sigs)} signatures chargées")
+    for s in sigs:
+        print(f" {s['type']}: {s['magic_bytes'].hex(' ').upper()}")
     path = sys.argv[1]
     header = read_header(path)
     # .hex(" ") affiche les octets séparés par des espaces.
     print(f"Header de {path}: {header.hex(' ').upper()}") #.hex converti en hexa
-    
+
